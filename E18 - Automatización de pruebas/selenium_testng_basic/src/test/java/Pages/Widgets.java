3e23c4c3a9a8243b075ecba4d5d36c2d5dd0c7cd
JuanCarlosPerezSanchez <152633822+JuanCarlosPerezSanchez@users.noreply.github.com>
2024-05-10 22:23:35 +0200

    Actualización

--- E18 - Automatización de pruebas/selenium_testng_basic/src/test/java/Pages/Widgets.java
+++ E18 - Automatización de pruebas/selenium_testng_basic/src/test/java/Pages/Widgets.java
@@ -1,6 +1,9 @@
 package Pages;
 
 import Utilities.Utils;
+
+import java.sql.Date;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
@@ -41,18 +44,27 @@ public class Widgets {
    * 
    **************************************************/
 
+
+
   /**
-   * Acceder a la pantalla de elementos
+   * Verificar el funcionamiento de "Date Picker 
    * @throws InterruptedException 
    */
-  @Test(description = "Widgets_TC04 - Acceder a la pantalla de widgets", enabled = false)
+  @Test(description = "Widgets_TC04 - Verificar el funcionamiento de \"Date Picker", enabled = true)
   public void Widgets_TC04() throws InterruptedException {
+
     Widgets_TC01();
+
+    //Paso 4
+    //Acción
     Utils.chromeDriver.findElementByXPath("//span[@class='text' and text()='Accordian']").click();
-    if(Utils.chromeDriver.findElementsByXPath("//li//span[text()='Text Box']").size() == 0 ||
-      Utils.chromeDriver.findElementsByXPath("//li//span[text()='Upload and Download']").size() == 0 ||
-      Utils.chromeDriver.findElementsByXPath("//li//span[text()='Dynamic Properties']").size() == 0){
-      System.out.println("Esta validación no bloqueante ha fallado!!");
+    // Thread.sleep(5000);
+    //Condición
+    if(Utils.chromeDriver.findElementsByXPath("//div[@class='react-datepicker__input-container']/input[@id='datePickerMonthYearInput']").contains(class) &&
+      Utils.chromeDriver.findElementsByXPath("").size() == 0 &&
+      Utils.chromeDriver.findElementsByXPath("").size() == 0)
+    {
+    System.out.println("Esta validación no bloqueante ha fallado!!");
     }
   }
 }
